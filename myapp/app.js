596e477b55c7159304cda1565aedb8380e946f7b
Stan Whitcomb <stanwhitcomb@outlook.com>
2017-02-22 16:03:55 -0800

    added mongo DB as well as a new user page

--- myapp/app.js
+++ myapp/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,48 +12,9 @@ var config = require('./config')();
 
 var app = express();
 
-// all the pages routes by get -- will add posts and other such if we need it later
-app.get('/', function(req, res) { res.render('index', {title : 'Main Page'})})
-// methods need to support: GET
-app.get('/main', function(req, res) { res.render('index', {title : 'Main Page'})})
-// methods need to support: GET
-app.get('/agreement', function(req, res) { res.render('agreement', {title: 'Agreement'})})
-// methods need to support: GET POST
-app.route('/chat') 
-    .get(function(req, res) { res.render('chat', {title: 'Chat'})})
-    .post(function(req, res) { res.send('should be the chat post page')})
-// methods need to support: GET POST
-app.route('/roster') 
-    .get(function(req, res) { res.render('roster', {title: 'Roster'})})
-    .post(function(req, res) { res.send('should be the roster post page')})
-// methods need to support: GET POST
-app.route('/tips') 
-    .get(function(req, res) { res.render('tips', {title: 'Tips'})})
-    .post(function(req, res) { res.send('should be the tips post page')})
-// methods need to support: GET POST
-app.route('/login') 
-    .get(function(req, res) { res.render('login', {title: 'Login'})})
-    .post(function(req, res) { res.send('should be the login post page')})
-// methods need to support: GET POST
-app.route('/calendar') 
-    .get(function(req, res) { res.render('calendar', {title: 'Calendar'})})
-    .post(function(req, res) { res.send('should be the calendar post page')})
-
 // start the app listening on port 3000
 app.listen(config.port, function() { console.log("listening on port " + config.port)})
-/*
-MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/peinv', function(err, db) {
-    if (err) { 
-        console.log('Sorry, no db running'); 
-    } else {
-    var attachDB = function(req, res, next) {
-        req.db = db;
-        next();
-    };
-    app.listen(config.port, function() { console.log("listening on port " + config.port)})
-    }
-});
-*/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,95 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// all the pages routes by get -- will add posts and other such if we need it later
+router.get('/', function(req, res) { res.redirect('/login') })
+ 
+// methods need to support: GET
+router.get('/main', function(req, res) { res.render('index', {title : 'Main Page'})})
 
+// methods need to support: GET
+router.get('/agreement', function(req, res) { res.render('agreement', {title: 'Agreement'})})
+
+// methods need to support: GET POST
+router.route('/chat') 
+    .get(function(req, res) { res.render('chat', {title: 'Chat'})})
+    .post(function(req, res) { res.send('should be the chat post page')})
+    
+// methods need to support: GET POST
+router.route('/roster') 
+    .get(function(req, res) { 
+        var MongoClient = mongo.MongoClient;
+        var url = 'mongodb://localhost:27017/peinv';
+        MongoClient.connect(url, function(err, db) {
+            if(err){
+                console.log('unable to connect to DB', err);
+            } else {
+                console.log('Connected');
+                var collection = db.collection('roster');
+                    
+                collection.find({}).toArray(function(err, result){
+                    if(err){
+                        res.send(err);
+                    } else if (result.length) {
+                        console.log(result);
+                        res.render('roster', {title: 'Roster', memberlist: result})
+                    } else {
+                        res.send("No DB info Found");
+                }});
+                db.close();
+            }});
+        })
+        
+    .post(function(req, res) { res.send('should be the roster post page')})
+    
+// methods need to support: GET POST
+router.route('/tips') 
+    .get(function(req, res) { res.render('tips', {title: 'Tips'})})
+    .post(function(req, res) { res.send('should be the tips post page')})
+    
+// methods need to support: GET POST
+router.route('/login') 
+    .get(function(req, res) { res.render('login', {title: 'Login'})})
+    .post(function(req, res) { res.redirect('/main')})
+
+router.route('/new-user') 
+    .get(function(req, res) { res.render('newUser', {title: 'New User Sign Up'})})
+    .post(function(req, res) { 
+        var MongoClient = mongo.MongoClient;
+        var url = 'mongodb://localhost:27017/peinv';
+        MongoClient.connect(url, function(err, db) {
+            if(err){
+                console.log('unable to connect to DB', err);
+            } else {
+                console.log('Connected');
+                var collection = db.collection('roster');
+                console.log("here");
+                
+                var new_member = {
+                    name: req.body.User,
+                    address: req.body.Address,
+                    password: req.body.Password,
+                    phone: req.body.Phone,
+                    spouse: req.body.Spouse,
+                    email: req.body.Email,
+                    birthday: req.body.Birthday,
+                };
+                console.log("and here");
+                console.log(new_member);
+                collection.insert([new_member], function(err, result){
+                if (err){
+                    console.log(err);
+                } else {
+                    res.redirect('roster')
+                }
+                db.close();
+            });
+            }});
+    })
+    
+// methods need to support: GET POST
+router.route('/calendar') 
+    .get(function(req, res) { res.render('calendar', {title: 'Calendar'})})
+    .post(function(req, res) { res.send('should be the calendar post page')})
 module.exports = router;
